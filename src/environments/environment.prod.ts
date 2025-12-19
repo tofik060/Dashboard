04b3889b96e2a78a276bc2db31f26e7d05e0a22c
Tofik <tofikansari@idcletech.com>
2025-12-19 21:00:31 +0530

    Fix: Increase Angular bundle size budget limits for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://e-mart-dashboard-a5a42owdi-tofik-hussain-ansaris-projects.vercel.app'  // TODO: Replace with your actual backend Vercel URL after deployment
+  apiUrl: 'https://e-mart-dashboard.vercel.app/'  // TODO: Replace with your actual backend Vercel URL after deployment
 };
 
